f82e1d25b7354647871643edc0bb2cf212da8ca5
Bibek Pant <pantbibek21@gmail.com>
2024-06-15 20:59:42 +0545

    feat: add load more functionality

--- src/components/OrganizationsCardContainer.jsx
+++ src/components/OrganizationsCardContainer.jsx
@@ -1,24 +1,42 @@
 import "../styles/global.css";
-import Card from "@layouts/Card.jsx";  
+import Card from "@layouts/Card.jsx";
 import styles from "../styles/components/OrganizationsCardContainer.module.css";
+import { useState, useEffect } from "react";
 
 // Sample organization JSON data
 import organizationJSON from "../data/organizations.json";
 
 const OrganizationsCardContainer = () => {
-  return (
-    <div className={`${styles["section-container"]} section-container`}>
-      <div className={styles["card-wrapper"]}>
-        {
-          organizationJSON.map((organization, index) => (
-            <Card key={index} data={organization} />
-          ))
-        }
-      </div>
+	let [organizationCounter, setOrganizationCounter] = useState(12);
+	let [isBtnVisible, setIsBtnVisible] = useState(true);
 
-      <button className={styles["load-more-btn"]}>Load More</button>
-    </div>
-  );
+	function handleLoadMore() {
+		setOrganizationCounter((prevCounter) => prevCounter + 4);
+	}
+
+	useEffect(() => {
+		if (organizationCounter >= organizationJSON.length) {
+			setIsBtnVisible(false);
+		}
+	}, [organizationCounter]);
+
+	return (
+		<div className={`${styles["section-container"]} section-container`}>
+			<div className={styles["card-wrapper"]}>
+				{organizationJSON
+					.slice(0, organizationCounter)
+					.map((organization, index) => (
+						<Card key={index} data={organization} />
+					))}
+			</div>
+
+			{isBtnVisible && (
+				<button className={styles["load-more-btn"]} onClick={handleLoadMore}>
+					Load More
+				</button>
+			)}
+		</div>
+	);
 };
 
 export default OrganizationsCardContainer;
