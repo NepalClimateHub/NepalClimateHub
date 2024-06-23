be14b04df6439e8194378e351a72611db426cc36
Bibek Pant <pantbibek21@gmail.com>
2024-06-23 15:26:08 +0545

    feat: add a function to sort newsJSON array with date

---
+++ src/utils/sortNewsArray.js
@@ -0,0 +1,22 @@
+export default function sortByBirthDateDescending(data) {
+  const monthShortNames = {
+      "Jan": "01",
+      "Feb": "02",
+      "Mar": "03",
+      "Apr": "04",
+      "May": "05",
+      "Jun": "06",
+      "Jul": "07",
+      "Aug": "08",
+      "Sep": "09",
+      "Oct": "10",
+      "Nov": "11",
+      "Dec": "12"
+  };
+  
+  return data.sort((a, b) => {
+      const dateA = new Date(a.date.replace(/(\w{3}) (\d{2}), (\d{4})/, (match, p1, p2, p3) => `${p3}-${monthShortNames[p1]}-${p2}`));
+      const dateB = new Date(b.date.replace(/(\w{3}) (\d{2}), (\d{4})/, (match, p1, p2, p3) => `${p3}-${monthShortNames[p1]}-${p2}`));
+      return dateB - dateA;
+  });
+}
