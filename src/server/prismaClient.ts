116b244dd7de4460056b85b2cbd375b2af877674
Bibek Pant <pantbibek21@gmail.com>
2024-07-06 19:41:17 +0545

    feat: create a new file for Prisma configuration for Neon database

--- src/server/prismaClient.ts
+++ src/server/prismaClient.ts
@@ -11,3 +11,5 @@ const connectionString = `${process.env.DATABASE_URL}`;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool);
 const prisma = new PrismaClient({ adapter });
+
+export default prisma;
