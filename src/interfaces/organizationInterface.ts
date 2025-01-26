fa244cd96e732f49914b774d1a76cd2f6b148ab1
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2025-01-26 13:13:42 +0545

    feat: update CardContainer export and refactor organization and activist pages for improved readability

--- src/interfaces/organizationInterface.ts
+++ src/interfaces/organizationInterface.ts
@@ -1,24 +1,24 @@
 // define the interfaces for the data structure
 export interface Contact {
-	email: string;
-	phone: string;
+  email: string;
+  phone: string;
 }
 
 export interface Socials {
-	facebook: string;
-	linkedin: string;
-	instagram: string;
+  facebook: string;
+  linkedin: string;
+  instagram: string;
 }
 
 export interface Organization {
-	id: number;
-	name: string;
-	description: string;
-	address: string;
-	contact: Contact;
-	socials: Socials;
-	website: string;
-	tags: string[];
-	logoUrl: string;
-	pictures: string[];
+  id: number;
+  name: string;
+  description: string;
+  address: string;
+  contact: Contact;
+  socials: Socials;
+  website: string;
+  tags: string[];
+  logoUrl: string;
+  pictures: string[];
 }
--- src/utils/isValidEmail.ts
+++ src/utils/isValidEmail.ts
@@ -1,4 +1,4 @@
 export default function isValidEmail(email) {
-	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	return emailRegex.test(email);
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 }
