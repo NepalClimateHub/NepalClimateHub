db4fb287e84adcd0f03c481a419d6e8ce7e4062f
Nischal <tamangnischal2018@gmail.com>
2025-08-23 17:32:26 +0545

    fixes all issue and added new page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 import net0Integration from './toolbar/integration.ts';
@@ -21,7 +20,16 @@ export default defineConfig({
   integrations: [net0Integration, sitemap(), [icon()], react()],
   adapter: netlify(),
   vite: {
-    plugins: [tailwindcss()],
     envPrefix: 'PUBLIC_',
+    resolve: {
+      alias: {
+        '@layouts': '/src/layouts',
+        '@components': '/src/components',
+        '@assets': '/src/assets',
+        '@i18n': '/src/i18n',
+        '@styles': '/src/styles',
+        '@pages': '/src/pages'
+      }
+    }
   },
 });
