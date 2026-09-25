194a62d171aa52bbe06404971316d6f7fc64eddb
Nischal Tamang <107337213+tmgnisc@users.noreply.github.com>
2026-09-25 19:57:14 +0545

    Comment out net0Integration import

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 import { defineConfig, envField } from 'astro/config';
-import net0Integration from './toolbar/integration.ts';
+// import net0Integration from './toolbar/integration.ts';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
@@ -38,7 +38,7 @@ export default defineConfig({
     },
   },
   integrations: [
-    net0Integration,
+
     sitemap({
       customSitemaps: [
         'https://nepalclimatehub.org/blogs/sitemap.xml',
